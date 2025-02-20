0284eba5be65bedd1895db5cfa80fbae47f1ae78
morgan <95859784+MorganGuille@users.noreply.github.com>
2025-02-20 09:44:26 +0100

    connect to mongodbatlas

--- server/index.js
+++ server/index.js
@@ -4,30 +4,23 @@ const mongoose = require("mongoose")
 const cors = require('cors');
 require('dotenv').config()
 
-let adminPassword = process.env.admin_password;
-
-
-
+let dbPassword = process.env.atlasDB_password;
 
 app.use(require("express").json())
 app.use(require("express").urlencoded({ extended: true }))
 app.use(cors())
 
-
 async function connecting() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/bookingsdb')
+        await mongoose.connect(`mongodb+srv://morganguille:${dbPassword}@bookingsdb.7bty4.mongodb.net/?retryWrites=true&w=majority&appName=BookingsDB`)
         console.log('Connected to the DB')
     } catch (error) {
         console.log('ERROR: Seems like your DB is not running, please start it up !!!');
     }
-
 }
 connecting()
 
 app.use("/bookings", require("./routes/bookings"))
 app.use("/admin", require("./routes/admin"))
-
-
 
 app.listen(port, () => console.log(`listening on port : ${port}`))
