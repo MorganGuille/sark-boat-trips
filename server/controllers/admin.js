66b18874e781c78eb21c02f6656c84ed03b2fce6
morgan <95859784+MorganGuille@users.noreply.github.com>
2025-02-28 13:06:15 +0100

    pushing to heroku

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -2,9 +2,7 @@ const Bookings = require("../models/bookings");
 
 
 const checklogin = async (req, res) => {
-    console.log("accessed")
     let { userName, password } = req.body
-    console.log(req.body)
     try {
         if (userName === process.env.admin_username && password === process.env.admin_password) {
             res.send({ ok: true, data: true })
