e594595b5f14e6b178e0d80a7a672cfcc39b3c91
morgan <95859784+MorganGuille@users.noreply.github.com>
2025-02-28 10:53:26 +0100

    change URL

--- client/config.js
+++ client/config.js
@@ -1,5 +1,9 @@
-const URL = window.location.hostname === `localhost`
-    ? `http://localhost:4040`
-    : `https://secret-eyrie-44762-a52d7fb9dbaa.herokuapp.com`
+// const URL = window.location.hostname === `localhost`
+//     ? `http://localhost:4040`
+//     : `https://secret-eyrie-44762-a52d7fb9dbaa.herokuapp.com`
+
+// export { URL }
+
+const URL = `https://secret-eyrie-44762-a52d7fb9dbaa.herokuapp.com`
 
 export { URL }
