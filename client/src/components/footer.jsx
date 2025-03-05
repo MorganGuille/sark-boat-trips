77c4c55c6ef0dc857808ad771191ae9d7de0d5b0
morgan <95859784+MorganGuille@users.noreply.github.com>
2025-03-05 12:43:54 +0100

    footer css and links

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -5,6 +5,13 @@ import '../css/footer.css'
 import PrivacyPolicy from './privacyPolicy'
 import Terms from './terms'
 
+import googleLogo from '../assets/logos/7123025_logo_google_g_icon.png'
+import tripLogo from '../assets/logos/4375113_logo_tripadvisor_icon.png'
+import faceLogo from '../assets/logos/317746_facebook_social media_social_icon.png'
+import instaLogo from '../assets/logos/4102579_applications_instagram_media_social_icon.png'
+
+
+
 function Footer() {
 
     const [showPrivacyP, setshowPrivacyP] = useState(null)
@@ -31,6 +38,13 @@ function Footer() {
                 <a onClick={() => setshowPrivacyP(!showPrivacyP)}>Privacy Policy</a>
                 <a onClick={() => setshowTerms(!showTerms)}>Terms and Conditions</a>
             </div>
+            <fieldset className='footer-links'>
+                <legend>Social links</legend>
+                <a href="https://www.facebook.com/sarkboattrips" target="_blank"><img width={'32px'} src={faceLogo} /></a>
+                <a href="https://www.instagram.com/sark_boat_trips/" target="_blank"><img width={'32px'} src={instaLogo} /></a>
+                <a href='https://www.tripadvisor.com/Attraction_Review-g186231-d6673939-Reviews-Sark_Boat_Trips-Sark_Channel_Islands.html' target="_blank"><img width={'32px'} src={tripLogo} /></a>
+                <a href='https://g.co/kgs/RsJniax' target="_blank"><img width={'32px'} src={googleLogo} /></a>
+            </fieldset>
         </footer>
 
     </>)
